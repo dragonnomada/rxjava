96f94d24a921e14534e805f98490a4d6b50a2b27
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-20 22:55:44 -0600

    Update n203

--- LabRxJava/src/main/java/com/nomadacode/Rx203Chaining.java
+++ LabRxJava/src/main/java/com/nomadacode/Rx203Chaining.java
@@ -1,2 +1,21 @@
-package com.nomadacode;public class Rx203Chaining {
+package com.nomadacode;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class Rx203Chaining {
+
+    public static void main(String[] args) {
+
+        Observable.just("Uno", "Dos", "Tres", "Cuatro", "Cinco")
+                .map(String::length)
+                .subscribe(System.out::println);
+
+        Observable.just(1, 2, 3, 4, 5, 6)
+                .map(x -> Math.pow(x, 2))
+                .map(y -> (float)(y / Math.PI))
+                .map(z -> (int)Math.abs(z))
+                .subscribe(System.out::println);
+
+    }
+
 }
