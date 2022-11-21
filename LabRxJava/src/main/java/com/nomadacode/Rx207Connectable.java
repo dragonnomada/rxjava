a3e9f75e81007ac1955464e97265239243add852
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-21 15:37:31 -0600

    Update n207

--- LabRxJava/src/main/java/com/nomadacode/Rx207Connectable.java
+++ LabRxJava/src/main/java/com/nomadacode/Rx207Connectable.java
@@ -1,2 +1,20 @@
-package com.nomadacode;public class Rx207Connectable {
+package com.nomadacode;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.observables.ConnectableObservable;
+
+public class Rx207Connectable {
+
+    public static void main(String[] args) {
+
+        ConnectableObservable<String> source = Observable.just("Alfa", "Beta", "Gama", "Delta", "Epsilon").publish();
+
+        source.subscribe(System.out::println);
+        source.map(String::toUpperCase).subscribe(System.out::println);
+        source.map(String::length).subscribe(System.out::println);
+
+        source.connect();
+
+    }
+
 }
