8dcff57c8c864968ed9d9746c3ff0bc3d6744cea
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-24 01:15:31 -0600

    Update c305

--- LabRxJava/src/main/java/com/nomadacode/Rx305BooleanOperators.java
+++ LabRxJava/src/main/java/com/nomadacode/Rx305BooleanOperators.java
@@ -1,10 +1,41 @@
 package com.nomadacode;
 
+import io.reactivex.rxjava3.core.Observable;
+
 public class Rx305BooleanOperators {
 
     public static void main(String[] args) {
 
+        System.out.println("all()");
 
+        Observable.range(1, 5)
+                .all(i -> i % 2 == 0)
+                .subscribe(System.out::println);
+
+        System.out.println("any()");
+
+        Observable.range(1, 5)
+                .any(i -> i % 2 == 0)
+                .subscribe(System.out::println);
+
+        System.out.println("isEmpty()");
+
+        Observable.range(1, 0)
+                .isEmpty()
+                .subscribe(System.out::println);
+
+        System.out.println("contains()");
+
+        Observable.range(1, 5)
+                .contains(3)
+                .subscribe(System.out::println);
+
+        System.out.println("sequenceEqual()");
+
+        Observable.sequenceEqual(
+                Observable.range(1, 5),
+                Observable.just(1, 2, 3, 4, 5)
+        ).subscribe(System.out::println);
 
     }
 
