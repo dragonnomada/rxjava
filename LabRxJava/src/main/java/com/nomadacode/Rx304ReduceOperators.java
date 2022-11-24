17d48835e76bdcfd484f41f8778766bc3f11b79c
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-24 01:08:03 -0600

    Update n304

--- LabRxJava/src/main/java/com/nomadacode/Rx304ReduceOperators.java
+++ LabRxJava/src/main/java/com/nomadacode/Rx304ReduceOperators.java
@@ -1,2 +1,24 @@
-package com.nomadacode;public class Rx304ReduceOperators {
+package com.nomadacode;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class Rx304ReduceOperators {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("count()");
+
+        Observable.range(1, 5)
+                .count()
+                .subscribe(System.out::println);
+
+        System.out.println("reduce()");
+
+        Observable.range(1, 5)
+                .reduce((accum, i) -> accum + i)
+                .subscribe(System.out::println);
+
+    }
+
 }
