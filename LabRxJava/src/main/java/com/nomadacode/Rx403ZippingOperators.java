3f5e0b5f648445cdd4d3f23dbee58f4f06e32001
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-25 00:17:02 -0600

    Update n403

--- LabRxJava/src/main/java/com/nomadacode/Rx403ZippingOperators.java
+++ LabRxJava/src/main/java/com/nomadacode/Rx403ZippingOperators.java
@@ -1,2 +1,22 @@
-package com.nomadacode;public class Rx403ZippingOperators {
+package com.nomadacode;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+public class Rx403ZippingOperators {
+
+    public static void main(String[] args) {
+
+        Observable.zip(
+                Observable.just("A", "B", "C").repeat(2),
+                Observable.interval(1, TimeUnit.SECONDS),
+                Observable.just(true, false, true).repeat(2),
+                (s1, s2, s3) -> String.format("[%s | %d | %B]", s1, s2, s3)
+        ).subscribe(System.out::println);
+
+        Sleep.sleep(5000);
+
+    }
+
 }
