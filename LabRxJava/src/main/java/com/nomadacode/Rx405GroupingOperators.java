9032007d993a1aa2a12404452113e420f32acb89
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-25 00:41:01 -0600

    Update n405

--- LabRxJava/src/main/java/com/nomadacode/Rx405GroupingOperators.java
+++ LabRxJava/src/main/java/com/nomadacode/Rx405GroupingOperators.java
@@ -1,4 +1,16 @@
 package com.nomadacode;
 
-public class Rx405GrouppingOperators {
+import io.reactivex.rxjava3.core.Observable;
+
+public class Rx405GroupingOperators {
+
+    public static void main(String[] args) {
+
+        Observable.range(1, 100)
+                .groupBy(i -> i % 4)
+                .flatMapSingle(group -> group.toList())
+                .subscribe(System.out::println);
+
+    }
+
 }
