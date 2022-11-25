a95e5f53ff1e5ba2739769ce6dfea809b02803a5
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-25 00:05:27 -0600

    Update n402

--- LabRxJava/src/main/java/com/nomadacode/Rx402AmbigousOperators.java
+++ LabRxJava/src/main/java/com/nomadacode/Rx402AmbigousOperators.java
@@ -1,2 +1,28 @@
-package com.nomadacode;public class Rx402AmbigousOperators {
+package com.nomadacode;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+public class Rx402AmbigousOperators {
+
+    public static void main(String[] args) {
+
+        Observable.amb(
+                        Arrays.asList(
+                                Observable.interval(300, TimeUnit.MILLISECONDS)
+                                        .map(i -> String.format("Source 1: %d", i))
+                                        .flatMap(i -> Observable.just(i).delay((long)(Math.random() * 100), TimeUnit.MILLISECONDS)),
+                                Observable.interval(300, TimeUnit.MILLISECONDS)
+                                        .map(i -> String.format("Source 2: %d", i))
+                                        .flatMap(i -> Observable.just(i).delay((long)(Math.random() * 100), TimeUnit.MILLISECONDS))
+                        )
+                )
+                .subscribe(System.out::println);
+
+        Sleep.sleep(2000);
+
+    }
+
 }
