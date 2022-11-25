76e7aa4f88d017475c49b81672d65ad65e219214
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-25 00:32:20 -0600

    Update n404

--- LabRxJava/src/main/java/com/nomadacode/Rx404CombiningLastOperators.java
+++ LabRxJava/src/main/java/com/nomadacode/Rx404CombiningLastOperators.java
@@ -1,2 +1,37 @@
-package com.nomadacode;public class Rx404CombiningLastOperators {
+package com.nomadacode;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.Disposable;
+
+import java.util.concurrent.TimeUnit;
+
+public class Rx404CombiningLastOperators {
+
+    public static void main(String[] args) {
+
+        Disposable disposable = Observable.combineLatest(
+                Observable.interval(1200, TimeUnit.MILLISECONDS).take(3),
+                Observable.interval(600, TimeUnit.MILLISECONDS).take(6),
+                Observable.interval(300, TimeUnit.MILLISECONDS).take(12),
+                (s1, s2, s3) -> String.format("[%d | %d | %d]", s1, s2, s3)
+        ).subscribe(System.out::println);
+
+        Sleep.sleep(4000);
+
+        disposable.dispose();
+
+        System.out.println("---------------------------------");
+
+        Observable.interval(1200, TimeUnit.MILLISECONDS)
+                .take(3)
+                .withLatestFrom(
+                        Observable.interval(600, TimeUnit.MILLISECONDS).take(6),
+                        Observable.interval(300, TimeUnit.MILLISECONDS).take(12),
+                        (s1, s2, s3) -> String.format("[%d | %d | %d]", s1, s2, s3)
+                ).subscribe(System.out::println);
+
+        Sleep.sleep(4000);
+
+    }
+
 }
