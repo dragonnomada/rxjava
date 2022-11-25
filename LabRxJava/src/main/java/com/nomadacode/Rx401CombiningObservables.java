1448a2621ce92526950d601f7ec88aa9480f308b
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-24 23:52:37 -0600

    Update n401

--- LabRxJava/src/main/java/com/nomadacode/Rx401CombiningObservables.java
+++ LabRxJava/src/main/java/com/nomadacode/Rx401CombiningObservables.java
@@ -1,2 +1,43 @@
-package com.nomadacode;public class Rx401CombiningObservables {
+package com.nomadacode;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+class Sleep {
+    static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class Rx401CombiningObservables {
+
+    public static void main(String[] args) {
+
+        Observable.merge(
+                Observable.just("A", "B", "C"),
+                Observable.interval(1, TimeUnit.SECONDS)
+                        .take(3)
+                        .map(i -> String.format("[%d]", i)),
+                Observable.just("D", "E", "F")
+        ).subscribe(System.out::println);
+
+        Sleep.sleep(4000);
+
+        Observable.concat(
+                Observable.just("A", "B", "C"),
+                Observable.interval(1, TimeUnit.SECONDS)
+                        .take(3)
+                        .map(i -> String.format("[%d]", i)),
+                Observable.just("D", "E", "F")
+        ).subscribe(System.out::println);
+
+        Sleep.sleep(4000);
+
+    }
+
 }
