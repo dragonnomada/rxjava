22582b403546b8cbcd8e4abbcf144603745abe5b
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-27 19:54:10 -0600

    Update n504

--- projects/LabRxJava/src/main/java/com/nomadacode/Rx504Caching.java
+++ projects/LabRxJava/src/main/java/com/nomadacode/Rx504Caching.java
@@ -1,2 +1,33 @@
-package com.nomadacode;public class Rx504Caching {
+package com.nomadacode;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+public class Rx504Caching {
+
+    public static void main(String[] args) {
+
+        Observable<String> sourceInterval = Observable.interval(1, TimeUnit.SECONDS)
+                .map(i -> {
+                    LocalDateTime dateObj = LocalDateTime.now();
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+                    String date = dateObj.format(formatter);
+                    return String.format("Emisión %d a las %s", i, date);
+                })
+                .take(5)
+                .cache();
+
+        sourceInterval.subscribe(s -> System.out.printf("[1] %s %n", s));
+
+        Sleep.sleep(5000);
+
+        sourceInterval.subscribe(s -> System.out.printf("[2] %s %n", s));
+
+
+
+    }
+
 }
