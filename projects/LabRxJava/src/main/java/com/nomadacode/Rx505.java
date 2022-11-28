2c48587c6e8af545db432fd7013fed097fc7ea85
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-27 20:32:31 -0600

    Update n505

--- projects/LabRxJava/src/main/java/com/nomadacode/Rx505.java
+++ projects/LabRxJava/src/main/java/com/nomadacode/Rx505.java
@@ -1,2 +1,27 @@
-package com.nomadacode;public class Rx505 {
+package com.nomadacode;
+
+import io.reactivex.rxjava3.subjects.PublishSubject;
+import io.reactivex.rxjava3.subjects.Subject;
+
+public class Rx505 {
+
+    public static void main(String[] args) {
+
+        // (1) Creamos el Subject que representa un dispositivo de emisiones
+        Subject<Integer> subject = PublishSubject.create();
+
+        // (2) Suscribimos un observador
+        subject.map(i -> String.format("<< %d >>", i))
+                .subscribe(s -> System.out.printf("[1]: %s %n", s));
+
+        // (3) Propagamos las emisiones
+        for (int i = 1; i <= 5; i++) {
+            subject.onNext(i);
+        }
+
+        // (4) Notificamos que las emisiones se han completado
+        subject.onComplete();
+
+    }
+
 }
