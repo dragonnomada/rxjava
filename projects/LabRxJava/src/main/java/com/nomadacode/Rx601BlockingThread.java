a54e1899b6ac9c9f6be5ea3670d00d4d124dba9e
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-27 22:23:09 -0600

    Update c601

---
+++ projects/LabRxJava/src/main/java/com/nomadacode/Rx601BlockingThread.java
@@ -0,0 +1,27 @@
+package com.nomadacode;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class Rx601Concurrency {
+
+    public static void main(String[] args) {
+
+        Observable<Integer> observable = Observable.range(1, 5)
+                .map(i -> {
+                    // Bloqueamos de 1 a 2 segundos
+                    Sleep.sleep((long) (Math.random() * 1000 + 1000));
+                    return i;
+                });
+
+        Util.logDate();
+
+        // El primer observador bloqueará el programa de 5 a 10 segundos
+        observable.subscribe(i -> System.out.printf("[1]: %d %n", i));
+        // El primer observador bloqueará el programa de 5 a 10 segundos
+        observable.subscribe(i -> System.out.printf("[2]: %d %n", i));
+
+        Util.logDate();
+
+    }
+
+}
