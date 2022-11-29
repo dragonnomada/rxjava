4ac1b4471cd0ce35e87e96067878db65b9a7300b
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-28 21:41:13 -0600

    Update c701

---
+++ projects/LabRxJava/src/main/java/com/nomadacode/Rx701BufferFile.java
@@ -0,0 +1,48 @@
+package com.nomadacode;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.subjects.PublishSubject;
+import io.reactivex.rxjava3.subjects.Subject;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Rx701Buffer {
+
+    static void readLines(String fileName, Subject<String> subject) {
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(new File(fileName));
+        } catch (FileNotFoundException e) {
+            subject.onError(e);
+            return;
+        }
+
+        while (scanner.hasNextLine()) {
+            subject.onNext(scanner.nextLine());
+        }
+
+        subject.onComplete();
+
+        scanner.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Subject<String> lines = PublishSubject.create();
+
+        lines.map(line -> line.split("\\s"))
+                .flatMap(parts ->
+                        Observable.fromArray(parts)
+                                .map(part -> Integer.parseInt(part))
+                )
+                .buffer(5)
+                .subscribe(System.out::println);
+
+        readLines("D:\\Datasets\\data.txt", lines);
+
+    }
+
+}
