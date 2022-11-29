2f2643177300f1d008ccfb5d10b4b52545a28187
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-28 23:44:39 -0600

    Update n704

--- projects/LabRxJava/src/main/java/com/nomadacode/Rx704Switching.java
+++ projects/LabRxJava/src/main/java/com/nomadacode/Rx704Switching.java
@@ -1,2 +1,29 @@
-package com.nomadacode;public class Rx704Switching {
+package com.nomadacode;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import io.reactivex.rxjava3.schedulers.Timed;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class Rx704Switching {
+
+    public static void main(String[] args) {
+
+        Observable<Timed<Integer>> feed = Observable.range(1, 100)
+                .subscribeOn(Schedulers.computation())
+                .concatMap(i ->
+                        Observable.just(i).delay((long) (Math.random() * 3000), TimeUnit.MILLISECONDS)
+                )
+                .timeInterval();
+
+        //source.blockingSubscribe(System.out::println);
+
+        Observable.interval(5, TimeUnit.SECONDS)
+                .switchMap(i -> feed.doOnDispose(() -> System.out.println("LIBERADO")))
+                .blockingSubscribe(System.out::println);
+
+    }
+
 }
