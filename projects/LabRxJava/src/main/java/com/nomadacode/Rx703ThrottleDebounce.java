0da91db47de5d9068e2c6abaff701615e14373dc
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-28 23:06:02 -0600

    Update n703

--- projects/LabRxJava/src/main/java/com/nomadacode/Rx703ThrottleDebounce.java
+++ projects/LabRxJava/src/main/java/com/nomadacode/Rx703ThrottleDebounce.java
@@ -1,2 +1,30 @@
-package com.nomadacode;public class Rx703ThrottleDebounce {
+package com.nomadacode;
+
+import io.reactivex.rxjava3.subjects.PublishSubject;
+import io.reactivex.rxjava3.subjects.Subject;
+
+import java.util.concurrent.TimeUnit;
+
+public class Rx703ThrottleDebounce {
+
+    public static void main(String[] args) {
+
+        Subject<Long> timer = PublishSubject.create();
+
+        timer.timeInterval()
+                .throttleWithTimeout(2, TimeUnit.SECONDS)
+                .subscribe(System.out::println);
+
+        Thread t = new Thread(() -> {
+            long count = 0;
+            while (true) {
+                timer.onNext(count++);
+                Sleep.sleep((long) (Math.random() * 3000));
+            }
+        });
+
+        t.start();
+
+    }
+
 }
--- projects/LabRxJava/src/main/java/com/nomadacode/Rx703ThrottleFirst.java
+++ projects/LabRxJava/src/main/java/com/nomadacode/Rx703ThrottleFirst.java
@@ -4,13 +4,13 @@ import io.reactivex.rxjava3.core.Observable;
 
 import java.util.concurrent.TimeUnit;
 
-public class Rx703ThrottleLast {
+public class Rx703ThrottleFirst {
 
     public static void main(String[] args) {
 
         Observable.interval(200, TimeUnit.MILLISECONDS)
                 .map(i -> i * 200)
-                .throttleLast(1, TimeUnit.SECONDS)
+                .throttleFirst(1, TimeUnit.SECONDS)
                 .blockingSubscribe(System.out::println);
 
     }
