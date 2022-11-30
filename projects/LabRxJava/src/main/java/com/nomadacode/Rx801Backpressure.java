9d605f31c2cec1c853b2a92a289d237b34a07b27
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-29 22:44:52 -0600

    Update n80x

--- projects/LabRxJava/src/main/java/com/nomadacode/Rx801Backpressure.java
+++ projects/LabRxJava/src/main/java/com/nomadacode/Rx801Backpressure.java
@@ -1,2 +1,38 @@
-package com.nomadacode;public class Rx801Backpressure {
+package com.nomadacode;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+import java.util.concurrent.TimeUnit;
+
+public class Rx801Backpressure {
+
+    public static void main(String[] args) {
+
+        class Product {
+            long id;
+
+            public Product(long id) {
+                this.id = id;
+                System.out.printf("Producto %d generado %n", id);
+            }
+
+            @Override
+            public String toString() {
+                return String.format("Soy el producto: %d", id);
+            }
+        }
+
+        Observable.interval(10, TimeUnit.MILLISECONDS)
+                .map(Product::new)
+                .observeOn(Schedulers.io())
+                .subscribe(product -> {
+                    Sleep.sleep(1000);
+                    System.out.printf("PRODUCTO PROCESADO: %s %n", product);
+                });
+
+        Sleep.sleep(10000);
+
+    }
+
 }
--- projects/LabRxJava/src/main/java/com/nomadacode/Rx803FlowableSubscriber.java
+++ projects/LabRxJava/src/main/java/com/nomadacode/Rx803FlowableSubscriber.java
@@ -1,2 +1,56 @@
-package com.nomadacode;public class Rx803FlowableSubscriber {
+package com.nomadacode;
+
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Rx803FlowableSubscriber {
+
+    public static void main(String[] args) {
+
+        Flowable.range(1, 1_000)
+                .doOnNext(i -> System.out.printf("RECIBIDO: %d %n", i))
+                .observeOn(Schedulers.io())
+                .map(i -> {
+                    Sleep.sleep(50);
+                    return i;
+                })
+                .subscribe(new Subscriber<Integer>() {
+                    Subscription subscription;
+                    AtomicInteger countRequests = new AtomicInteger(1);
+
+                    @Override
+                    public void onSubscribe(Subscription subscription) {
+                        this.subscription = subscription;
+                        subscription.request(40);
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Sleep.sleep(50);
+                        System.out.printf("RECIBIDO: %d (%d) %n", integer, countRequests.get());
+                        if (countRequests.getAndIncrement() % 20 == 0 && countRequests.get() >= 40) {
+                            System.out.printf("Solicitando 20 emisiones más (%d) %n", countRequests.get());
+                            subscription.request(20);
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        throwable.printStackTrace();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("FLUJO FINALIZADO");
+                    }
+                });
+
+        Sleep.sleep(20000);
+
+    }
+
 }
