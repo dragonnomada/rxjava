becdc4cddf765f7c40ea43b2978c0f5eeb8eeaa6
Alan Badillo Salas <dragonnomada123@gmail.com>
2022-11-29 23:03:19 -0600

    Update n80x

--- projects/LabRxJava/src/main/java/com/nomadacode/Rx804BackpressureBuffer.java
+++ projects/LabRxJava/src/main/java/com/nomadacode/Rx804BackpressureBuffer.java
@@ -1,2 +1,31 @@
-package com.nomadacode;public class Rx804BackpressureBuffer {
+package com.nomadacode;
+
+import io.reactivex.rxjava3.core.BackpressureOverflowStrategy;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+import java.util.concurrent.TimeUnit;
+
+public class Rx804BackpressureBuffer {
+
+    public static void main(String[] args) {
+
+        Flowable.interval(1, TimeUnit.MILLISECONDS)
+                .onBackpressureBuffer(3,
+                        () -> {},
+                        //() -> System.out.println("Buffer superado"),
+                        BackpressureOverflowStrategy.DROP_LATEST
+                        //BackpressureOverflowStrategy.DROP_OLDEST
+                        //BackpressureOverflowStrategy.ERROR
+                )
+                .observeOn(Schedulers.io())
+                .subscribe(i -> {
+                    Sleep.sleep(50);
+                    System.out.printf("RECIBIDO: %d %n", i);
+                });
+
+        Sleep.sleep(30000);
+
+    }
+
 }
